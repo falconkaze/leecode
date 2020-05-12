e8917938d752a1427373b382c2c87e8a0a91ad9f
falconkaze <ykqyao@qq.com>
2020-05-13 00:02:27 +0800

    [leetcode] offer51 数组中的逆序对

---
+++ leetcode/剑指offer/051/reverse_pairs_v1.c
@@ -0,0 +1,11 @@
+int reversePairs(int* nums, int numsSize){
+    int sum = 0;
+    for(int i = 0; i < numsSize - 1; i ++){
+        for(int j = i + 1; j < numsSize; j ++){
+            if(nums[i] > nums[j]){
+                sum ++;
+            }
+        }
+    }
+    return sum;
+}
